646e1d09826f677836fdd6f20c7c741d332d5a1f
vasanthvaka <vs.vaka22@gmail.com>
2025-07-02 12:28:21 +0530

    updated

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = process.env.VITE_API_URL;
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
