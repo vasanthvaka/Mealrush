b161d8e30a0aa72190a3ca33dd6279db7c23715f
vasanthvaka <vs.vaka22@gmail.com>
2025-07-02 14:10:32 +0530

    updated

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,15 +5,14 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
-export const currency = '₹'
+export const url = import.meta.env.VITE_API_URL;
+export const currency = '₹';
 
-export const assets ={
+export const assets = {
     logo,
     add_icon,
     order_icon,
     profile_image,
     upload_area,
     parcel_icon
-}
-
+};
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,3 +1,5 @@
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
